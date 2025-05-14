e83ae82d4ff99c168bcaac2d929f76807e203d9d
sean <qaz1996001@gmail.com>
2025-05-14 12:50:39 +0800

    delete pipeline_raw_diom_to_nii_inference.py pipeline_raw_diom_to_nii_inference.sh

--- code_ai/pipeline/pipeline_raw_diom_to_nii_inference.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#export PYTHONPATH=$(dirname $(dirname "$PWD"))
-#echo PYTHONPATH
-export PYTHONPATH=$(dirname $(dirname "$(cd "$(dirname "$0")" && pwd)"))
-echo "PYTHONPATH:$PYTHONPATH"
-python3 "$PYTHONPATH/code_ai/pipeline/pipeline_raw_diom_to_nii_inference.py" "$@"
