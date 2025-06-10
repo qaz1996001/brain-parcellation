09ce37e154aa41e9073fb88df61cacbe6eaee580
sean <qaz1996001@gmail.com>
2025-06-10 18:14:12 +0800

    rerun __init__.py model.py routers.py schemas.py service.py urls.py

---
+++ backend/app/rerun/urls.py
@@ -0,0 +1,4 @@
+prefix                     = "/rerun"
+RERUN_PROT_STUDY_UID       = f"{prefix}/study/by-uid"
+RERUN_PROT_STUDY_RENAME_ID = f"{prefix}/study/by-rename_id"
+RERUN_PROT_STUDY_FAIL      = f"{prefix}/study/fail"
