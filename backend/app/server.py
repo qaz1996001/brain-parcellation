dd260602b82284c3da8270c38612c31f62e3e6dd
sean <qaz1996001@gmail.com>
2025-06-24 23:29:07 +0800

    app/service.py   init_cache

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,10 +1,28 @@
 # app/server.py
+import os
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
+
+
+
+async def init_cache():
+    REDIS_HOST     = os.getenv("REDIS_HOST")
+    REDIS_USERNAME = os.getenv("REDIS_USERNAME")
+    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+    REDIS_PORT     = os.getenv("REDIS_PORT")
+    REDIS_DB       = os.getenv("REDIS_DB_FASTAPI_CACHE",6)
+
+    REDIS_URL = f'redis://{REDIS_USERNAME}:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}'
+    redis = aioredis.from_url(REDIS_URL, encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
 
 from .routers import router
 from .database import alchemy
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,6 +34,7 @@ async def lifespan(app: FastAPI):
     """
 
     # Startup event
+    await init_cache()
     # asyncio.create_task(task_scheduler.start())
     yield
     # Shutdown event
