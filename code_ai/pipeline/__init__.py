f655c59758c123d5bc24d6673988e7a8d14aa4b3
sean <qaz1996001@gmail.com>
2025-06-24 17:15:47 +0800

    20250624 __init__.py chuan_code dir bug sean 2025/6/18 下午 03:42

--- code_ai/pipeline/__init__.py
+++ code_ai/pipeline/__init__.py
@@ -44,19 +44,20 @@ class PipelineConfig:
     def __init__(self, script_name, data_key):
         self.script_name = script_name
         self.data_key = data_key
-    #
+
+
     def generate_cmd(self, study_id: str, task: Task, input_dicom_dir: Optional[str] = None):
         input_path_list = [str(x) for x in task.input_path_list]
         output_path = os.path.dirname(task.output_path)
-        PATH_ROOT   = pathlib.Path(os.getenv('PATH_ROOT'))
-        chuan_root  = PATH_ROOT.parent.joinpath('chuan')
+        PATH_ROOT = pathlib.Path(os.getenv('PATH_ROOT'))
+        chuan_root = PATH_ROOT.parent.joinpath('chuan')
         chuan_code = chuan_root.joinpath('code')
         # PATH_ROOT = / mnt / e / pipeline / sean
         if self.data_key == 'Aneurysm':
             # pipeline_aneurysm_tensorflow.py [-h] [--ID ID]
-#                                        [--Inputs INPUTS [INPUTS ...]]
-#                                        [--DicomDir DICOMDIR [DICOMDIR ...]]
-#                                        [--Output_folder OUTPUT_FOLDE
+            #                                        [--Inputs INPUTS [INPUTS ...]]
+            #                                        [--DicomDir DICOMDIR [DICOMDIR ...]]
+            #                                        [--Output_folder OUTPUT_FOLDE
             if input_dicom_dir is None:
                 return (f'cd {str(chuan_code)}  && '
                         f'{self.python3} {self.script_name} '
@@ -64,8 +65,8 @@ class PipelineConfig:
                         f'--Inputs {" ".join(input_path_list)} '
                         f'--Output_folder {output_path} ')
             else:
-                return (f'cd {str(chuan_root)}  && '
-                        f'{self.python3} code/{self.script_name} '
+                return (f'cd {str(chuan_code)}  && '
+                        f'{self.python3} {self.script_name} '
                         f'--ID {study_id} '
                         f'--Inputs {" ".join(input_path_list)} '
                         f'--Output_folder {output_path} '
