324764d41dd0efc0b5bf594f03a231019f6c0915
sean <qaz1996001@gmail.com>
2025-06-26 13:19:41 +0800

    - backend/app/sync/service.py -- _send_events --- logger

--- backend/app/sync/service.py
+++ backend/app/sync/service.py
@@ -215,8 +215,9 @@ class DCOPEventDicomService(BaseRepositoryService[DCOPEventModel]):
         """
         async with httpx.AsyncClient(timeout=180) as client:
             url = f"{api_url}{SYNC_PROT_OPE_NO}"
-            event_data_json = json.dumps(event_data)
-            await client.post(url=url, timeout=180, json=event_data_json)
+            # event_data_json = json.dumps(event_data)
+            logger.info(f'_send_events {event_data}')
+            await client.post(url=url, timeout=180, data=event_data)
 
     async def _initiate_conversion_process(
             self,
