c2b675fcbcb112ed5fa252d6fd84b47ee57a46b0
sean <qaz1996001@gmail.com>
2025-07-02 13:15:06 +0800

    -code_ai/pipeline -- __init__.py fix import

--- code_ai/pipeline/__init__.py
+++ code_ai/pipeline/__init__.py
@@ -1,13 +1,11 @@
 import argparse
-import json
 import os
 import pathlib
-import subprocess
 from typing import Optional
-
-from code_ai import PYTHON3
 from code_ai.utils import study_id_pattern
 from code_ai.utils.inference import InferenceEnum, Task
+from code_ai.pipeline.upload import upload_json,platform_json
+from code_ai.pipeline.dicomseg import dicom_seg_cmb_file
 
 
 def get_study_id(file_name: str) -> Optional[str]:
@@ -90,92 +88,11 @@ class PipelineConfig:
 
 
 pipelines = {
-    InferenceEnum.Aneurysm: PipelineConfig('pipeline_aneurysm_tensorflow.py', 'Aneurysm'),
-    # InferenceEnum.Area: PipelineConfig('pipeline_synthseg_tensorflow.py', 'Area'),
-    # InferenceEnum.CMB: PipelineConfig('pipeline_cmb_tensorflow.py', 'CMB'),
+    # InferenceEnum.Aneurysm: PipelineConfig('pipeline_aneurysm_tensorflow.py', 'Aneurysm'),
+    InferenceEnum.Area: PipelineConfig('pipeline_synthseg_tensorflow.py', 'Area'),
+    InferenceEnum.CMB: PipelineConfig('pipeline_cmb_tensorflow.py', 'CMB'),
     # InferenceEnum.DWI: PipelineConfig('pipeline_synthseg_dwi_tensorflow.py', 'DWI'),
     # InferenceEnum.WMH_PVS: PipelineConfig('pipeline_synthseg_wmh_tensorflow.py', 'WMH_PVS'),
     # InferenceEnum.WMH: PipelineConfig('pipeline_wmh_tensorflow.py', 'WMH'),
     # InferenceEnum.Infarct: PipelineConfig('pipeline_infarct_tensorflow.py', 'Infarct'),
     }
-
-
-def dicom_seg_multi_file(ID:str,
-                         InputsDicomDir:str,
-                         nii_path_str:str,
-                         path_output:str):
-    cmd_str = ('export PYTHONPATH={} && '
-               '{} code_ai/pipeline/dicomseg/create_dicomseg_multi_file_json_claude.py '
-               '--ID {} '
-               '--InputsDicomDir {} '
-               '--Inputs {} '
-               '--Output_folder {} '.format(pathlib.Path(__file__).parent.parent.parent.absolute(),
-                                            PYTHON3,
-                                            ID,
-                                            InputsDicomDir,
-                                            nii_path_str,
-                                            path_output)
-               )
-
-    process = subprocess.Popen(args=cmd_str, shell=True,
-                               # cwd='{}'.format(pathlib.Path(__file__).parent.parent.absolute()),
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    print('dicom_seg_multi_file',cmd_str)
-    return stdout, stderr
-
-
-def upload_dicom_seg(input_dicom_seg_folder:str,input_nifti:str):
-    input_dicom_seg_folder_path = pathlib.Path(input_dicom_seg_folder)
-    input_nifti_path = pathlib.Path(input_nifti)
-    dicom_seg_base_name = input_nifti_path.name.split('.')[0]
-    file_list = sorted(input_dicom_seg_folder_path.rglob(dicom_seg_base_name + '*.dcm'))
-    file_str_list = list(map(lambda x: str(x), file_list))
-    cmd_str = ('export PYTHONPATH={} && '
-               '{} code_ai/pipeline/upload/orthanc_dicom.py '
-               '--Input {} '.format(pathlib.Path(__file__).parent.parent.parent.absolute(),
-                                    PYTHON3,
-                                    ' '.join(file_str_list)
-                                    )
-               )
-    print('upload_dicom_seg',cmd_str)
-
-    process = subprocess.Popen(args=cmd_str, shell=True,
-                               # cwd='{}'.format(pathlib.Path(__file__).parent.parent.absolute()),
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    return stdout, stderr
-
-
-def upload_json(ID: str, mode: InferenceEnum) -> object:
-    path_process = os.getenv("PATH_PROCESS")
-    cmd_json_path_path = os.path.join(path_process, 'Deep_cmd_tools', '{}_cmd.json'.format(ID))
-    if os.path.exists(cmd_json_path_path):
-        with open(cmd_json_path_path, 'r') as f:
-            data_json = json.load(f)
-        cmd_data = next(filter(lambda x: x['study_id'] == ID and x['name'] == mode, data_json))
-        if cmd_data is not None:
-            file_list = cmd_data['output_list']
-            nii_file_list = list(filter(lambda x: str(x).endswith('nii.gz'), file_list))
-            platform_json_list = list(map(lambda x: str(x).replace('.nii.gz', '_platform_json.json'), nii_file_list))
-
-            platform_json_list = list(filter(lambda x: os.path.exists(x), platform_json_list))
-            for platform_json in platform_json_list:
-                cmd_str = ('export PYTHONPATH={} && '
-                           '{} code_ai/pipeline/upload/platform_json.py '
-                           '--Input {} '.format(pathlib.Path(__file__).parent.parent.parent.absolute(),
-                                                PYTHON3,
-                                                platform_json
-                                                )
-                           )
-                print('upload_json', cmd_str)
-
-                process = subprocess.Popen(args=cmd_str, shell=True,
-                                           # cwd='{}'.format(pathlib.Path(__file__).parent.parent.absolute()),
-                                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                stdout, stderr = process.communicate()
-                print(stdout, stderr)
-        else:
-            raise ValueError('No found {} for {}'.format(mode, ID))
-    else:
-        raise FileNotFoundError(cmd_json_path_path)
