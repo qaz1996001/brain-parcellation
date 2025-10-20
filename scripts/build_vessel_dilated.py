2b3fe59ef4947d24b6a50feb4b0186753928c32d
sean <qaz1996001@gmail.com>
2025-10-20 10:45:12 +0800

    build_vessel_dilated.py

--- scripts/build_vessel_dilated.py
+++ scripts/build_vessel_dilated.py
@@ -1,90 +1,7 @@
 import argparse
+import os
 import pathlib
-import pydicom
 from code_ai.pipeline.rdx.build import VesselDilatedBuilder
-from code_ai.pipeline.dicomseg import utils
-
-
-
-def execute_rdx_platform_json(_id: int, path_root: pathlib.Path,
-                              model_id: str = '289fc383-34ff-46b7-bf0a-fbb22a104a18'):
-    """
-    執行 RDX 平台 JSON 生成流程，處理動脈瘤檢測的 DICOM 和預測結果
-
-    Args:
-        _id: 患者或案例 ID
-        path_root: 資料根目錄路徑
-        model_id: 模型識別碼，預設為動脈瘤檢測模型 ID
-    """
-
-    # 定義所有必要的資料夾路徑
-    path_dict = dict(
-        path_dcms=path_root.joinpath("Dicom"),  # DICOM 原始檔案目錄
-        path_nii=path_root.joinpath("Image_nii"),  # NIfTI 格式影像目錄
-        path_reslice_nii=path_root.joinpath("Image_reslice"),  # 重切影像目錄
-        path_dcmseg=path_root.joinpath("Dicom", "Dicom-Seg")  # DICOM-SEG 輸出目錄
-    )
-
-    # 初始化動脈瘤檢測 JSON 建構器
-    platform_json_builder = VesselDilatedBuilder()
-
-    # 定義三個序列名稱：腦部 MRA、俯仰角 MIP、偏航角 MIP
-    series_name_list = ['MRA_BRAIN']
-
-    # 讀取每個序列的 DICOM 資料夾路徑列表
-    path_dcms_folder_path_list = [path_dict['path_dcms'].joinpath(x) for x in series_name_list]
-
-    # 建立預測 NIfTI 檔案路徑列表
-    # MRA_BRAIN 使用 Image_nii 目錄，其他使用 Image_reslice 目錄
-    pred_nii_path_list = [{"series_name": x,
-                           "pred_nii_path": str(path_dict['path_nii'].joinpath("Vessel.nii.gz"))
-                           if x == 'MRA_BRAIN'
-                           else str(path_dict['path_reslice_nii'].joinpath("{}_vessel.nii.gz".format(x)))
-                           }
-                          for x in series_name_list]
-
-    # 載入並排序每個序列的 DICOM 檔案
-    #             - sorted_dcms: List of sorted DICOM file paths
-    #             - image: SimpleITK image object
-    #             - first_dcm: First DICOM dataset
-    #             - source_images: List of all DICOM datasets (without pixel data)
-    dicom_data_list = [utils.load_and_sort_dicom_files(x) for x in path_dcms_folder_path_list]
-    series_first_dcm_data_list = [x[2] for x in dicom_data_list]
-
-    pred_result_list = [{"series_name": x[0],
-                         "data": platform_json_builder.use_create_dicom_seg_file(
-                             path_dict['path_nii'] if x[0] == 'MRA_BRAIN' else path_dict['path_reslice_nii'],
-                             x[0],       # sorted_dcms
-                             path_dict['path_dcmseg'],
-                             *x[1][1:]  # 解包 DICOM 資料（跳過第一個元素）
-                         )}
-                        for x in zip(series_name_list, dicom_data_list)]
-
-    # 讀取所有生成的 DICOM-SEG 檔案
-    dcm_seg_path_list = [list(map(lambda xx: pydicom.read_file(xx['dcm_seg_path']), x['data']))
-                         for x in pred_result_list]
-
-    # 使用建構器模式組裝最終的平台 JSON
-    aneurysm_platform_json = (platform_json_builder
-                              .set_patient_info(series_first_dcm_data_list)                     # 設定患者資訊
-                              .set_model_id(model_id)                                         # 設定模型 ID
-                              .set_detections(dcm_seg_path_list[0], pred_result_list)  # 設定檢測結果
-                              .build()  # 建構最終 JSON
-                              )
-
-    # 確保輸出目錄存在
-    output_series_folder = path_root
-    if output_series_folder.is_dir():
-        output_series_folder.mkdir(exist_ok=True, parents=True)
-    else:
-        output_series_folder.parent.mkdir(parents=True, exist_ok=True)
-
-    # 儲存平台 JSON 檔案
-    platform_json_path = output_series_folder.joinpath(path_root.joinpath('rdx_vessel_dilated_json.json'))
-    with open(platform_json_path, 'w') as f:
-        f.write(aneurysm_platform_json.model_dump_json())
-
-    print("Processing complete!")
 
 
 def main():
@@ -111,8 +28,12 @@ def main():
 
     path_root = pathlib.Path(path_processModel)
     path_processID = path_root.joinpath( args.id)
-    execute_rdx_platform_json(_id=args.id,
-                              path_root=path_processID)
+
+    print('path_processID',os.environ.get('path_processID'))
+    # platform_json = VesselDilatedBuilder.execute_rdx_platform_json(args.id, path_processID)
+    # print('platform_json',platform_json)
+    # execute_rdx_platform_json(_id=args.id,
+    #                           path_root=path_processID)
 
 
 
