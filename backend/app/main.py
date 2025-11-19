035c80485dbef457b3b25a470d90ab83d85177ed
sean <qaz1996001@gmail.com>
2025-11-19 13:48:17 +0800

    optimization import main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ from code_ai import load_dotenv
 
 
 if __name__ == "__main__":
-    from backend.app.server import app
+    # from backend.app.server import app
     load_dotenv()
     #
     APP_PORT = int(os.getenv("APP_PORT",8000))
