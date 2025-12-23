4f354be0d3176b82a75469dd45edbeb4a1de708f
sean <qaz1996001@gmail.com>
2025-12-23 22:28:29 +0800

    test init

--- code_ai/pipeline/pipeline_dicom_to_nii.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#export PYTHONPATH=$(dirname $(dirname "$PWD"))
-#echo PYTHONPATH
-export PYTHONPATH=$(dirname $(dirname "$(cd "$(dirname "$0")" && pwd)"))
-echo "PYTHONPATH:$PYTHONPATH"
-python3 "$PYTHONPATH/code_ai/pipeline/pipeline_dicom_to_nii.py" "$@"
-
