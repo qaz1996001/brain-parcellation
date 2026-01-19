70a1ef060cb7bb16eec02debe180b207ad970baf
sean <qaz1996001@gmail.com>
2026-01-19 12:18:19 +0800

    fix __init__.py

--- code_ai/pipeline/__init__.py
+++ code_ai/pipeline/__init__.py
@@ -1,10 +1,10 @@
 import argparse
 import os
 import pathlib
-from typing import Optional
+from typing import Optional, List, Callable
 from code_ai.utils import study_id_pattern
 from code_ai.utils.inference import InferenceEnum, Task
-from code_ai.pipeline.upload import upload_json,platform_json
+from code_ai.pipeline.upload import upload_json, platform_json
 from code_ai.pipeline.dicomseg import dicom_seg_cmb_file
 
 
@@ -15,24 +15,119 @@ def get_study_id(file_name: str) -> Optional[str]:
     return ""
 
 
-MODEL_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'resource', 'models')
+MODEL_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "resource", "models"
+)
 
 
 def pipeline_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ID', type=str, default='10516407_20231215_MR_21210200091',
-                        help='目前執行的case的patient_id or study id')
+    parser.add_argument(
+        "--ID",
+        type=str,
+        default="10516407_20231215_MR_21210200091",
+        help="目前執行的case的patient_id or study id",
+    )
 
-    parser.add_argument('--Inputs', type=str, nargs='+',
-                        default=['/mnt/e/rename_nifti_202505051/10516407_20231215_MR_21210200091/SWAN.nii.gz',
-                                 '/mnt/e/rename_nifti_202505051/10516407_20231215_MR_21210200091/T1BRAVO_AXI.nii.gz'],
-                        help='用於輸入的檔案')
-    parser.add_argument('--Output_folder', type=str, default='/mnt/d/wsl_ubuntu/pipeline/sean/example_output/',
-                        help='用於輸出結果的資料夾')
-    parser.add_argument('--InputsDicomDir', type=str,
-                        default='/mnt/e/rename_dicom_202505051/10516407_20231215_MR_21210200091/SWAN',
-                        help='用於輸入的檔案')
+    parser.add_argument(
+        "--Inputs",
+        type=str,
+        nargs="+",
+        default=[
+            "/mnt/e/rename_nifti_202505051/10516407_20231215_MR_21210200091/SWAN.nii.gz",
+            "/mnt/e/rename_nifti_202505051/10516407_20231215_MR_21210200091/T1BRAVO_AXI.nii.gz",
+        ],
+        help="用於輸入的檔案",
+    )
+    parser.add_argument(
+        "--Output_folder",
+        type=str,
+        default="/mnt/d/wsl_ubuntu/pipeline/sean/example_output/",
+        help="用於輸出結果的資料夾",
+    )
+    parser.add_argument(
+        "--InputsDicomDir",
+        type=str,
+        default=None,  # Linus: "Don't have magic defaults that point to test data"
+        help="用於輸入的檔案",
+    )
     return parser
+
+
+# class PipelineConfig:
+#     base_path = pathlib.Path(__file__).parent.parent.parent.absolute()
+#     python3 = os.getenv("PYTHON3")
+#     conda = "conda"
+#     conda_env = "tf_2_14"
+#     chuan_data_key_list = ["Aneurysm" ,"Infarct", "WMH"]
+#
+#     def __init__(self, script_name, data_key, batch_inputs: bool = False):
+#         self.script_name = script_name
+#         self.data_key = data_key
+#         # batch_inputs=True means all inputs are passed to one inference run
+#         # batch_inputs=False means each input runs a separate inference
+#         self.batch_inputs = batch_inputs
+#
+#     def generate_cmd(
+#         self,
+#         study_id: str,
+#         task: Task,
+#         input_dicom_dir: Optional[str] = None,
+#         path_root: Optional[str] = None,
+#     ):
+#         input_path_list = [str(x) for x in task.input_path_list]
+#         output_path = os.path.dirname(task.output_path)
+#         # Use path_root from parameter if given, otherwise fallback to environment variable
+#         # This enables dual deployment: backends can pass different path_root to shared worker
+#         if path_root is not None:
+#             PATH_ROOT = pathlib.Path(path_root)
+#         else:
+#             PATH_ROOT = pathlib.Path(os.getenv("PATH_ROOT"))
+#         chuan_root = PATH_ROOT.parent.joinpath("chuan")
+#         chuan_code = chuan_root.joinpath("code")
+#         # PATH_ROOT = / mnt / e / pipeline / sean
+#         if self.data_key in self.chuan_data_key_list:
+#             # pipeline_aneurysm_tensorflow.py [-h] [--ID ID]
+#             #                                        [--Inputs INPUTS [INPUTS ...]]
+#             #                                        [--DicomDir DICOMDIR [DICOMDIR ...]]
+#             #                                        [--Output_folder OUTPUT_FOLDE
+#             if input_dicom_dir is None:
+#                 return (
+#                     f"cd {str(chuan_code)}  && "
+#                     f"bash {str(chuan_code)}/{self.script_name} "
+#                     f"{study_id} "
+#                     f"{' '.join(input_path_list)} "
+#                     f"{task.output_path} "
+#                 )
+#             else:
+#                 return (
+#                     f"cd {str(chuan_code)}  && "
+#                     f"bash {str(chuan_code)}/{self.script_name} "
+#                     f"{study_id} "
+#                     f"{' '.join(input_path_list)} "
+#                     f"{input_dicom_dir} "
+#                     f"{task.output_path} "
+#                 )
+#         else:
+#             # Use absolute path to script so command works regardless of cwd
+#             script_path = self.base_path / "code_ai" / "pipeline" / self.script_name
+#             if input_dicom_dir is None:
+#                 return (
+#                     f"export PYTHONPATH={self.base_path} && "
+#                     f"{self.python3} {script_path} "
+#                     f"--ID {study_id} "
+#                     f"--Inputs {' '.join(input_path_list)} "
+#                     f"--Output_folder {output_path} "
+#                 )
+#             else:
+#                 return (
+#                     f"export PYTHONPATH={self.base_path} && "
+#                     f"{self.python3} {script_path} "
+#                     f"--ID {study_id} "
+#                     f"--Inputs {' '.join(input_path_list)} "
+#                     f"--Output_folder {output_path} "
+#                     f"--InputsDicomDir {input_dicom_dir} "
+#                 )
 
 
 class PipelineConfig:
